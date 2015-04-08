051a254eb74c3b8b3c733a89c0473ef2eab20ba4
Phil Johnston <support@mintplugins.com>
2015-04-08 01:31:11 -0400

    change from curl to wp_remote_get

--- includes/misc-functions/misc-functions.php
+++ includes/misc-functions/misc-functions.php
@@ -22,21 +22,9 @@ function mp_stacks_sociallinks_get_sociallinks_icons(){
 	$pattern = '/\.(mp-stacks-sociallinks-(?:\w+(?:-)?)+):before\s+{\s*content:\s*"(.+)";\s+}/';
 	//$subject = file_get_contents( plugins_url( '/fonts/font-awesome-4.0.3/css/font-awesome.css', dirname( __FILE__ ) ) );
 	
-	// Initializing curl
-	$ch = curl_init();
-	 
-	//Return Transfer
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	$return = wp_remote_get( plugins_url( '/fonts/fontello/css/fontello.css', dirname( __FILE__ ) ), array( 'sslverify' => false ) );
 	
-	//File to fetch
-	curl_setopt($ch, CURLOPT_URL, plugins_url( '/fonts/fontello/css/fontello.css', dirname( __FILE__ ) ) );
-											 
-	// Getting results
-	$subject =  curl_exec($ch); // Getting jSON result string
-	
-	curl_close($ch);
-	
-	preg_match_all($pattern, $subject, $matches, PREG_SET_ORDER);
+	preg_match_all($pattern, $return['body'], $matches, PREG_SET_ORDER);
 	
 	$icons = array();
 
