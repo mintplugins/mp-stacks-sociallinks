8ad1859c9655e4e09d1625bc5e5994be8a174f81
Phil Johnston <support@mintplugins.com>
2015-09-21 13:42:10 -0400

    Admin Meta Scripts now enqueued only when needed.

---
+++ includes/misc-functions/admin-enqueue-scripts.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * This file contains the enqueue scripts function for the sociallinks plugin
+ *
+ * @since 1.0.0
+ *
+ * @package    MP Stacks Features
+ * @subpackage Functions
+ *
+ * @copyright  Copyright (c) 2015, Mint Plugins
+ * @license    http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @author     Philip Johnston
+ */
+
+/**
+ * Enqueue Admin CSS for Ajax
+ *
+ * @since    1.0.0
+ * @link     http://mintplugins.com/doc/
+ * @see      function_name()
+ * @param  	 array $stylesheets An array containing the urls for each stylesheet as a key>value pair. Each key is what you'd use as the 'handle' in a wp_enqueue_scripts
+ * @return   array $stylesheets The incoming array with our additional stylesheet urls added. These will be added to the Brick Editor <head> upon ajax completion.
+ */
+function mp_stacks_sociallinks_ajax_admin_css( $stylesheets, $metabox_id ){
+	
+	if ( $metabox_id != 'mp_stacks_sociallinks_metabox' ){
+		return $stylesheets;	
+	}
+	
+	//Enqueue Admin CSS
+	$stylesheets['mp_stacks_sociallinks_css'] = plugins_url( 'css/admin-sociallinks.css?ver=' . MP_STACKS_SOCIALLINKS_VERSION, dirname( __FILE__ ) );
+	
+	//Enqueue Social Icons
+	$stylesheets['mp-stacks-sociallinks-icons'] = plugins_url( '/fonts/fontello/css/fontello.css?ver=' . MP_STACKS_SOCIALLINKS_VERSION, dirname( __FILE__ ) );
+	
+	return $stylesheets;
+
+}
+add_filter( 'mp_core_metabox_ajax_admin_css_stylesheets', 'mp_stacks_sociallinks_ajax_admin_css', 10, 2 );
