988fe3f0cdd5db1de518657f1205bb2b9996758b
Phil Johnston <support@mintplugins.com>
2015-09-21 13:30:05 -0400

    Brick Metabox controls now load using ajax.

--- includes/metaboxes/mp-stacks-sociallinks-meta/mp-stacks-sociallinks-meta.php
+++ includes/metaboxes/mp-stacks-sociallinks-meta/mp-stacks-sociallinks-meta.php
@@ -32,7 +32,8 @@ function mp_stacks_sociallinks_create_meta_box(){
 		'metabox_title' => __( '"SocialLinks" Content-Type', 'mp_stacks_sociallinks'), 
 		'metabox_posttype' => 'mp_brick', 
 		'metabox_context' => 'advanced', 
-		'metabox_priority' => 'low' 
+		'metabox_priority' => 'low' ,
+		'metabox_content_via_ajax' => true, 
 	);
 	
 	/**
@@ -211,4 +212,5 @@ function mp_stacks_sociallinks_create_meta_box(){
 	global $mp_stacks_sociallinks_meta_box;
 	$mp_stacks_sociallinks_meta_box = new MP_CORE_Metabox($mp_stacks_sociallinks_add_meta_box, $mp_stacks_sociallinks_items_array);
 }
-add_action('mp_brick_metabox', 'mp_stacks_sociallinks_create_meta_box');
+add_action('mp_brick_ajax_metabox', 'mp_stacks_sociallinks_create_meta_box');
+add_action('wp_ajax_mp_stacks_sociallinks_metabox_content', 'mp_stacks_sociallinks_create_meta_box');
